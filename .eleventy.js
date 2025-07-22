c13e9a64de0271a107a66bbfa6bbd866cb39a4ca
OmarMWarraich <earthling986@gmail.com>
2025-07-22 17:09:14 +0500

    feat: implement basic CSS styling and layout structure
    - Create src/style.css with foundational styles
    - Update layout.hbs with CSS link and semantic HTML
    - Configure Eleventy CSS passthrough copy
    - Addresses task 1 of issue #3

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,9 @@ const pluginHandlebars = require("@11ty/eleventy-plugin-handlebars");
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginHandlebars);
 
+  // Copy CSS files to output directory
+  eleventyConfig.addPassthroughCopy("src/style.css");
+
   return {
     dir: {
       input: "src",
